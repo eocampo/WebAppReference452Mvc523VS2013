5fd0adfd89271ac51baf8af89c2fce6bc920f8ac
Ernesto Ocampo <ernesto@sistrategia.com>
2017-08-22 09:26:25 -0500

    Initial template.

---
+++ WebAppReference452Mvc523VS2013/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppReference452Mvc523VS2013.MvcApplication" Language="C#" %>
---
+++ WebAppReference452Mvc523VS2013/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppReference452Mvc523VS2013.Startup))]
+namespace WebAppReference452Mvc523VS2013
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
